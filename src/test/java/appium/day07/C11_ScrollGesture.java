35f1d4ea6ee54c08722779a2682bacca43b93118
fatmakaa <142013126+fatmakaa@users.noreply.github.com>
2024-04-25 20:08:38 +0300

    me

---
+++ src/test/java/appium/day07/C11_ScrollGesture.java
@@ -0,0 +1,56 @@
+package appium.day07;
+
+import appium.basedtest.ApiDemosBasedTest;
+import com.google.common.collect.ImmutableMap;
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.testng.annotations.Test;
+
+public class C11_ScrollGesture extends ApiDemosBasedTest {
+
+
+    @Test
+    public void testScrollToElement() throws InterruptedException {
+        driver.findElement(AppiumBy.accessibilityId("Views")).click();
+
+        driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Layouts\"))"));
+        Thread.sleep(5000);
+    }
+
+    @Test
+    public void testScrollGestureElement() throws InterruptedException {
+        driver.findElement(AppiumBy.accessibilityId("Views")).click();
+
+        WebElement element = driver.findElement(AppiumBy.id("android:id/list"));
+
+        driver.executeScript("mobile: scrollGesture", ImmutableMap.of(
+                "elementId", ((RemoteWebElement) element).getId(),
+                "direction", "down",
+                "percent", 2.0,
+                "speed", 500
+        ));
+
+        Thread.sleep(5000);
+    }
+
+    @Test
+    public void testScrollGestureCoordinate() throws InterruptedException {
+        driver.findElement(AppiumBy.accessibilityId("Views")).click();
+
+        boolean canScrollMore = true;
+        while (canScrollMore) {
+            canScrollMore = (Boolean) driver.executeScript("mobile: scrollGesture", ImmutableMap.of(
+                    "left", 100, "top", 300, "width", 0, "height", 1000,
+                    "direction", "down",
+                    "percent", 1.0,
+                    "speed", 500
+            ));
+        }
+
+        Thread.sleep(5000);
+    }
+
+
+}
