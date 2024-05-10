8962516ee7bee011392f39824fde1702218d4044
fatmakaa <142013126+fatmakaa@users.noreply.github.com>
2024-05-10 14:00:09 +0300

    me

--- src/test/java/appium/day08practice/P06_KitapYurdu_ScrollSwipeGestures.java
+++ src/test/java/appium/day08practice/P06_KitapYurdu_ScrollSwipeGestures.java
@@ -1,7 +1,11 @@
 package appium.day08practice;
 
 import appium.basetest.KitapYurduBaseTest;
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class P06_KitapYurdu_ScrollSwipeGestures extends KitapYurduBaseTest {
 
@@ -15,10 +19,39 @@ public class P06_KitapYurdu_ScrollSwipeGestures extends KitapYurduBaseTest {
 
 
     @Test
-    public void testKitapYurdu(){
+    public void testKitapYurdu() throws InterruptedException {
+
+        driver.findElement(AppiumBy.id("com.mobisoft.kitapyurdu:id/btnLaterOn")).click();
+
+        WebElement swipeElement = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().resourceId(\"com.mobisoft.kitapyurdu:id/recyclerViewList\").instance(0)"));
+
+        swipeGesture(driver,swipeElement,"left",1.0,1000);
 
 
+        WebElement fifthBook = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().resourceId(\"com.mobisoft.kitapyurdu:id/imageViewProduct\").instance(1)"));
 
+         clickGesture(driver,fifthBook);
+
+
+        List<WebElement> bookInfo = driver.findElements(AppiumBy.xpath("//android.widget.ScrollView[@resource-id=\"com.mobisoft.kitapyurdu:id/productDetailScrollView\"]/android.view.ViewGroup/android.widget.TextView"));
+
+        System.out.println("Book Name:" + bookInfo.get(0).getText());
+        System.out.println("Author Name:" + bookInfo.get(1).getText());
+        System.out.println("Publisher Name:" + bookInfo.get(2).getText());
+        System.out.println("________________________________");
+
+
+        WebElement scrollElement = driver.findElement(AppiumBy.id("com.mobisoft.kitapyurdu:id/productDetailScrollView"));
+        scrollGesture(driver,scrollElement,"down",2.4,1000);
+
+
+        List<WebElement> kunyeTitle = driver.findElements(AppiumBy.id("com.mobisoft.kitapyurdu:id/textViewAttributeTitle"));
+
+        List<WebElement> kunyeInfo = driver.findElements(AppiumBy.id("com.mobisoft.kitapyurdu:id/textViewAttributeName"));
+        for (int i = 0; i < kunyeTitle.size(); i++) {
+            System.out.print(kunyeTitle.get(i).getText() + "\t");
+            System.out.println(kunyeInfo.get(i).getText());
+        }
 
 
     }
--- src/test/java/appium/day08practice/P07_Harita_PinchOpenAndClose.java
+++ src/test/java/appium/day08practice/P07_Harita_PinchOpenAndClose.java
@@ -1,6 +1,14 @@
 package appium.day08practice;
 
 import appium.basetest.HaritalarBaseTest;
+import com.google.common.collect.ImmutableMap;
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+
 
 public class P07_Harita_PinchOpenAndClose extends HaritalarBaseTest {
 
@@ -13,8 +21,60 @@ public class P07_Harita_PinchOpenAndClose extends HaritalarBaseTest {
     */
 
 
+    @Test
+    public void mapTest() throws InterruptedException {
+
+
+       WebElement layouts =  driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().className(\"android.widget.ImageView\").instance(8)"));
+       clickGesture(driver,layouts);
+
+       WebElement stateLine = driver.findElement(AppiumBy.id("com.google.android.apps.maps:id/layers_menu_satellite_layer"));
+        clickGesture(driver,stateLine);
+
+        WebElement close = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().className(\"android.widget.ImageView\").instance(0)"));
+        clickGesture(driver,close);
+
+        WebElement searchBox = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().text(\"Search here\")"));
+        clickGesture(driver,searchBox);
+
+        WebElement searchBoxx = driver.findElement(AppiumBy.id("com.google.android.apps.maps:id/search_omnibox_edit_text"));
+        searchBoxx.sendKeys("Taj Mahal");
+
+        KeyEvent enter = new KeyEvent(AndroidKey.ENTER);
+        driver.pressKey(enter);
+
+        Thread.sleep(3000);
+
+
+        driver.executeScript("mobile: pinchOpenGesture", ImmutableMap.of(
+                "left", 330,
+                "top", 560,
+                "width", 430,
+                "height", 500,
+                "percent", 0.75,
+                "speed", 300
+        ));
+
+        Thread.sleep(2000);
+
+        driver.executeScript("mobile: pinchCloseGesture", ImmutableMap.of(
+                "left", 330,
+                "top", 560,
+                "width", 430,
+                "height", 500,
+                "percent", 0.75,
+                "speed", 300
+        ));
 
 
 
+        Thread.sleep(5000);
+
+
+
+
+
+
+    }
 
 }
